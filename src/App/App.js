ff26e1137a1902f443c6a09c5ace24ba1684386a
Jeressia Williamson <jeressiajaytv@gmail.com>
2023-02-25 15:07:17 -0600

    added layout

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <div className="left-nav">Nav Bar Goes Here</div>
+      <div className='left-area'>Task Area Goes Here</div>
+      <div className='middle-area'>User Card + Game Area Goes Here</div>
+      <div className='right-area'>Chatbox Goes Here</div>
     </div>
   );
 }
