b6303f65fd5db73e5506e546e1dd849473fe1e41
Jeressia Williamson <jeressiajaytv@gmail.com>
2023-02-25 15:36:29 -0600

    user card done

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import './UserCard/UserCard'
+import UserCard from './UserCard/UserCard';
+
 
 function App() {
+  const user = {
+    userId: 1,
+    username: 'wesSnipes',
+    firstName: 'Wesley',
+    lastName: 'Snipes',
+    userStatus: 'Finance Newbie',
+    userImgUrl: 'http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQoNG-jW5p2OrTrpjk-DMtQlic1W2EWuh-QNKpNJTocQUrWPxt8AxtdCnwkXNVbDm2OYAD2dAOTAQzXR3Q'
+  }
   return (
     <div className="App">
-
+      <UserCard user={user} />
     </div>
   );
 }
