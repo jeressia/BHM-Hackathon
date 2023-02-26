ccd1a7c86a88c615900574dfaf60885670c7846c
Jeressia Williamson <jeressiajaytv@gmail.com>
2023-02-25 20:57:26 -0600

    collecting user data

--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import React from 'react'
 
+import SelectUSState from 'react-select-us-states';
+
+
 function Quiz() {
   const [userData, setUserData] = useState({})
   console.log("userData: ", userData)
@@ -18,6 +21,33 @@ function Quiz() {
         type="date"
         onChange={(e) => { setUserData({ ...userData, dateOfBirth: e.target.value }) }}
       />
+      <p>
+        Select a state: <SelectUSState id="myId" className="myClassName" onChange={(e) => {
+          setUserData({ ...userData, state: e })
+        }} />
+      </p>
+      <p>What is your greatest financial at the moment?</p>
+      <input type="radio" name="motivation" value="1" id="" onChange={(e) => {
+        setUserData({ ...userData, keyMotivationId: e.target.value })
+      }} />Purchase a House/Car
+      <input type="radio" name="motivation" value="2" id="" onChange={(e) => {
+        setUserData({ ...userData, keyMotivationId: e.target.value })
+      }} />Becoming debt free
+      <input type="radio" name="motivation" value="3" id="" onChange={(e) => {
+        setUserData({ ...userData, keyMotivationId: e.target.value })
+      }} />Increase your credit
+      <input type="radio" name="motivation" value="4" id="" onChange={(e) => {
+        setUserData({ ...userData, keyMotivationId: e.target.value })
+      }} />Saving Money
+      <input type="radio" name="motivation" value="5" id="" onChange={(e) => {
+        setUserData({ ...userData, keyMotivationId: e.target.value })
+      }} />Sticking to a budget
+      <input type="radio" name="motivation" value="6" id="" onChange={(e) => {
+        setUserData({ ...userData, keyMotivationId: e.target.value })
+      }} />Creating a new stream of income
+      <input type="radio" name="motivation" value="7" id="" onChange={(e) => {
+        setUserData({ ...userData, keyMotivationId: e.target.value })
+      }} />Retiring Early
     </div >
   )
 }
