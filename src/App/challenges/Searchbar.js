6a90a8bf21d97567bb4a8e3c7816be090e6ca97b
ivanedrei <ivanramirez2014@gmail.com>
2023-02-26 19:18:49 -0600

    working on searchbar to work propertly

--- src/App/challenges/Searchbar.js
+++ src/App/challenges/Searchbar.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 
-
 export const Searchbar = () => {
 
     const [searchInput, setSearchInput] = useState("");
@@ -12,7 +11,6 @@ export const Searchbar = () => {
         { id: 2, name: "Debt-Free" },
         { id: 3, name: "Credit Score Growth" },
         { id: 4, name: "Investment" }
-
     ];
 
     const handleChange = (e) => {
@@ -30,25 +28,51 @@ export const Searchbar = () => {
         });
     }
 
-    return <div>
+    return <>
+        <form>
+            <input
+                type="text"
+                placeholder="search"
+                value={searchInput}
+                onChange={handleChange}
+            />
+            <button type='submit'>
+                search
+            </button>
+        </form>
+        <ul>
+            {countries.map((i) => (
+                <li key={i.id}>{i.name}</li>
+            ))}
+        </ul>
+    </>
 
-        <input
-            type="search"
-            placeholder="Debt Free"
-            onChange={handleChange}
-            value={searchInput} />
 
-        <table>
+    // return <div>
 
-            <tbody>
-                {filteredCountries.map((country) => (
-                    <tr>
-                        <td>{country.name}</td>
-                    </tr>
-                ))}
-            </tbody>
+    //     <input
+    //         type="text"
+    //         placeholder="Debt Free"
+    //         onChange={handleChange}
+    //         value={searchInput} />
 
-        </table>
+    //     <table>
 
-    </div>
+    //         <tbody>
+    //             {filteredCountries.map((country) => (
+    //                 <tr>
+    //                     <td>{country.name}</td>
+    //                 </tr>
+    //             ))}
+    //         </tbody>
+
+    //     </table>
+    //     <form>
+    //         <button type='submit' >
+    //             <img src=''
+    //                 className='' />
+    //         </button>
+    //     </form>
+
+    // </div>
 }
