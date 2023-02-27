7a6bdfadb3a2977915b302de10bec90bf9cb7e6a
Jeressia Williamson <jeressiajaytv@gmail.com>
2023-02-26 20:35:35 -0600

    set profile picture

--- src/Chatbox/Chatbox.js
+++ src/Chatbox/Chatbox.js
@@ -9,7 +9,7 @@ function ChatBubble(props) {
                     {
                         (props.talker === 'moneybot') ?
                             <img src={MoneyBotIcon} alt={`${props.talker} avatar`} /> :
-                            <img src="https://www.looper.com/img/gallery/wesley-snipes-has-something-to-say-about-the-latest-blade-news/intro-1563907716.jpg" alt={`${props.talker} avatar`} />
+                            <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2022-03/220317-Cynthia-Talla-ew-321p-4bc199.jpg" alt={`${props.talker} avatar`} />
                     }
                 </div>
             </div>
