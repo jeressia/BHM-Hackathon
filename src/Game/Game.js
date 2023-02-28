0583c76bffedbe9ecceeca5c1918dac1a7d63aa7
Jeressia Williamson <jeressiajaytv@gmail.com>
2023-02-27 19:10:38 -0600

    added game progress bars for levels

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -4,16 +4,55 @@ import './Game.css';
 
 const Game = (props) => {
   const { userData } = props;
-  const [gameLevels, setGameLevels] = useState([])
-  const [progressPercent, setProgressPercent] = useState(25);
+  const [gameLevels, setGameLevels] = useState([
+    {
+      id: 1,
+      name: 'LVL 1',
+      percentToComplete: 10,
+      isComplete: false
+    },
+    {
+      id: 2,
+      name: 'LVL 2',
+      percentToComplete: 15,
+      isComplete: false
+    },
+    {
+      id: 3,
+      name: 'LVL 3',
+      percentToComplete: 15,
+      isComplete: false
+    },
+    {
+      id: 4,
+      name: 'LVL 4',
+      percentToComplete: 20,
+      isComplete: false
+    },
+    {
+      id: 5,
+      name: 'LVL 5',
+      percentToComplete: 25,
+      isComplete: false
+    }
+  ])
+  const [progressPercent, setProgressPercent] = useState(0);
 
   return (
     <div className="Game">
       <h1>{userData.userStatus}</h1>
-      <div className="level-indicator">LVL 1</div>
-      <progress role="progressbar" max="100" value={progressPercent}>
-      </progress>
-    </div>
+      <div className='all-levels'>
+        {gameLevels.map((level) => {
+          return (
+            <div className='level'>
+              <div className="level-indicator">{level.name}</div>
+              <progress role="progressbar" max={level.percentToComplete} value={level.isComplete ? level.percentToComplete : progressPercent}>
+              </progress>
+            </div>
+          )
+        })}
+      </div>
+    </div >
   )
 }
 export default Game
